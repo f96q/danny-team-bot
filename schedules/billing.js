8c9478b64aab5c35798aafe1f1f3c8d1a3e1cc87
danny <danny.dev8@gmail.com>
2017-09-03 00:59:16 +0900

    Add service billing

--- schedules/billing.js
+++ schedules/billing.js
@@ -6,22 +6,23 @@ const billing = require('../lib/billing')
 const Response = require('../robot/response')
 
 module.exports.billing = (event, context, callback) => {
-  billing((error, dataPoint) => {
+  billing((error, results) => {
     if (error) {
       console.error(error)
       return
     }
     const response = new Response(process.env.SCHEDULE_POST_CHANNEL)
-    if (dataPoint == null) {
+    if (results.length == 0) {
       response.send(__('schedules.billing.no_data_point'))
       return
     }
-    const attachments = [
-      {
-        title: __('schedules.billing.aws_billing'),
-        text: `$${dataPoint['Maximum']}`
-      }
-    ]
-    response.send(null, {attachments: attachments})
+    const fields = results.filter(result => {
+      return result.name != null
+    }).map(result => {
+      const datapoint = result.data['Datapoints'][0]
+      return { title: result.name, value: `$${datapoint['Maximum']}`, short: true }
+    })
+    const datapoint = results[0].data['Datapoints'][0]
+    response.send(__('schedules.billing.aws_billing', { value: `$${datapoint['Maximum']}` }), { attachments: [{ color: 'good', fields: fields }] })
   })
 }
--- scripts/billing.js
+++ scripts/billing.js
@@ -4,22 +4,23 @@ const billing = require('../lib/billing')
 
 module.exports = robot => {
   robot.hear(/billing/, 'billing - AWS billing', response => {
-    billing((error, dataPoint) => {
+    billing((error, results) => {
       if (error) {
         console.error(error)
         return
       }
-      if (dataPoint == null) {
+      if (results.length == 0) {
         response.send(__('schedules.billing.no_data_point'))
         return
       }
-      const attachments = [
-        {
-          title: __('schedules.billing.aws_billing'),
-          text: `$${dataPoint['Maximum']}`
-        }
-      ]
-      response.send(null, {attachments: attachments})
+      const fields = results.filter(result => {
+        return result.name != null
+      }).map(result => {
+        const datapoint = result.data['Datapoints'][0]
+        return { title: result.name, value: `$${datapoint['Maximum']}`, short: true }
+      })
+      const datapoint = results[0].data['Datapoints'][0]
+      response.send(__('schedules.billing.aws_billing', { value: `$${datapoint['Maximum']}` }), { attachments: [{ color: 'good', fields: fields }] })
     })
   })
 }
